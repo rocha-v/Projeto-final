8e19489b78f6673f7611792d4a2729a82286486d
Vanessa <85293484+rocha-v@users.noreply.github.com>
2023-08-02 18:22:12 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var texto = "Vanessa Rocha";
+var atraso = 200;
+var textoElemento = document.querySelector(".titulo-destaque");
+
+
+var contadorDeLetras = 0;
+function escreverTexto() {
+  if (contadorDeLetras < texto.length) {
+    textoElemento.textContent += texto.charAt(contadorDeLetras);
+    contadorDeLetras++;
+    setTimeout(escreverTexto, atraso);
+  }
+}
+
+escreverTexto();
+
+
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const buttonMenu = document.querySelector(".button__menu");
+const menuMobile = document.querySelector(".cabecalho__menu__mobile")
+let isMenuOpen = false;
+
+
+buttonMenu.addEventListener('click', function(){
+    isMenuOpen ? menuMobile.classList.add('invisivel'):menuMobile.classList.remove('invisivel')
+    
+    isMenuOpen ? buttonMenu.children[0].setAttribute('src','file:///C:/Users/Vanessa/Desktop/projeto-final/Projeto-final-main/Projeto-final-main/assets/menu.svg') :buttonMenu.children[0].setAttribute('src','file:///C:/Users/Vanessa/Desktop/projeto-final/Projeto-final-main/Projeto-final-main/assets/Close.svg');
+
+    isMenuOpen =! isMenuOpen;
+})
