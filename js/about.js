f8d2087fa917fdbec1f0cdefad45038aaeb72ddd
Vanessa <85293484+rocha-v@users.noreply.github.com>
2023-08-09 11:18:30 -0300

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,15 @@
+var texto = "Sobre mim";
+var atraso = 200;
+var textoElemento = document.querySelector(".titulo-destaque");
+
+
+var contadorDeLetras = 0;
+function escreverTexto() {
+  if (contadorDeLetras < texto.length) {
+    textoElemento.textContent += texto.charAt(contadorDeLetras);
+    contadorDeLetras++;
+    setTimeout(escreverTexto, atraso);
+  }
+}
+
+escreverTexto();
---
+++ js/contact.js
@@ -0,0 +1,15 @@
+var texto = "Entre em contato";
+var atraso = 200;
+var textoElemento = document.querySelector(".titulo-destaque");
+
+
+var contadorDeLetras = 0;
+function escreverTexto() {
+  if (contadorDeLetras < texto.length) {
+    textoElemento.textContent += texto.charAt(contadorDeLetras);
+    contadorDeLetras++;
+    setTimeout(escreverTexto, atraso);
+  }
+}
+
+escreverTexto();
